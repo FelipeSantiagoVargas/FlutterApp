7366fd89e7aff6b4f052beb74bff71fc9ab0dc31
FelipeVargas <FelipeVargas@software-genesis.com>
2022-07-15 10:53:14 -0500

    Primer cambio al repositorio

---
+++ linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,15 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/developer/snap/flutter/common/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/developer/apps/flutter/my_app" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/developer/snap/flutter/common/flutter"
+  "PROJECT_DIR=/home/developer/apps/flutter/my_app"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/developer/apps/flutter/my_app/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/developer/apps/flutter/my_app/lib/main.dart"
+)
